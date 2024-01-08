e3625c14a267f13fa7cee68b50381773fd564fbe
Spydnel <seth.h.liljenberg@gmail.com>
2024-01-08 17:03:13 +0100

    Initial commit

---
+++ src/main/java/net/spydnel/crawlers/CreepersCrawlersClient.java
@@ -0,0 +1,10 @@
+package net.spydnel.crawlers;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CreepersCrawlersClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
