5a8f9ecc1bf238808cf85ffaf48603343aa8be7f
Spydnel <seth.h.liljenberg@gmail.com>
2024-01-13 12:44:55 +0100

    Sitting

--- src/main/java/net/spydnel/crawlers/entity/custom/CrawlerEntity.java
+++ src/main/java/net/spydnel/crawlers/entity/custom/CrawlerEntity.java
@@ -1,14 +1,20 @@
 package net.spydnel.crawlers.entity.custom;
 
+import com.ibm.icu.impl.ValidIdentifiers;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.passive.CatEntity;
+import net.minecraft.entity.passive.FoxEntity;
 import net.minecraft.entity.passive.OcelotEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.world.World;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.core.animatable.GeoAnimatable;
@@ -17,9 +23,13 @@ import software.bernie.geckolib.core.animatable.instance.SingletonAnimatableInst
 import software.bernie.geckolib.core.animation.*;
 import software.bernie.geckolib.core.object.PlayState;
 
+import java.util.EnumSet;
+
 public class CrawlerEntity extends HostileEntity implements GeoEntity {
+    private static final TrackedData<Boolean> SITTING =
+            DataTracker.registerData(CrawlerEntity.class, TrackedDataHandlerRegistry.BOOLEAN);
     private AnimatableInstanceCache cache = new SingletonAnimatableInstanceCache(this);
-    public CrawlerEntity(EntityType<? extends HostileEntity> entityType, World world) {
+    public CrawlerEntity(EntityType<? extends CrawlerEntity> entityType, World world) {
         super(entityType, world);
     }
 
@@ -30,15 +40,60 @@ public class CrawlerEntity extends HostileEntity implements GeoEntity {
     }
 
     protected void initGoals() {
-        this.goalSelector.add(0, new SwimGoal(this));
+        this.goalSelector.add(0, new StopMoving());
+        this.goalSelector.add(1, new SwimGoal(this));
         this.goalSelector.add(2, new MeleeAttackGoal(this, 1.0, false));
-        this.goalSelector.add(3, new FleeEntityGoal(this, OcelotEntity.class, 6.0F, 1.0, 1.2));
-        this.goalSelector.add(3, new FleeEntityGoal(this, CatEntity.class, 6.0F, 1.0, 1.2));
+        this.goalSelector.add(3, new FleeEntityGoal<>(this, OcelotEntity.class, 6.0F, 1.0, 1.2));
+        this.goalSelector.add(3, new FleeEntityGoal<>(this, CatEntity.class, 6.0F, 1.0, 1.2));
         this.goalSelector.add(5, new WanderAroundFarGoal(this, 1.0));
         this.goalSelector.add(6, new LookAtEntityGoal(this, PlayerEntity.class, 6.0F));
         this.goalSelector.add(7, new LookAroundGoal(this));
 
         this.targetSelector.add(4, new ActiveTargetGoal<>(this, PlayerEntity.class, true));
+    }
+
+    public void writeCustomDataToNbt(NbtCompound nbt) {
+        super.writeCustomDataToNbt(nbt);
+        nbt.putBoolean("Sitting", this.isSitting());
+    }
+
+    public void readCustomDataFromNbt(NbtCompound nbt) {
+        super.readCustomDataFromNbt(nbt);
+        if (nbt.getBoolean("Sitting")) {
+            this.sit();
+        }
+    }
+
+    private class StopMoving extends Goal {
+        int timer;
+
+        public StopMoving() {
+            this.setControls(EnumSet.of(Control.LOOK, Control.JUMP, Control.MOVE));
+        }
+
+        public boolean canStart() {
+            return CrawlerEntity.this.isSitting();
+        }
+        public void start() {
+            CrawlerEntity.this.setJumping(false);
+            CrawlerEntity.this.getNavigation().stop();
+            CrawlerEntity.this.getMoveControl().moveTo(
+                    CrawlerEntity.this.getX(), CrawlerEntity.this.getY(), CrawlerEntity.this.getZ(), 0.0D
+            );
+        }
+    }
+    public void sit() {
+        this.dataTracker.set(SITTING, true);
+    }
+
+    public boolean isSitting() {
+        return this.dataTracker.get(SITTING);
+    }
+
+    @Override
+    protected void initDataTracker() {
+        super.initDataTracker();
+        this.dataTracker.startTracking(SITTING, false);
     }
 
     @Override
@@ -47,7 +102,10 @@ public class CrawlerEntity extends HostileEntity implements GeoEntity {
     }
 
     private PlayState predicate(AnimationState<GeoAnimatable> geoAnimatableAnimationState) {
-        if (geoAnimatableAnimationState.isMoving()) {
+        if (this.isSitting()) {
+            geoAnimatableAnimationState.getController().setAnimation(RawAnimation.begin().then("animation.crawler.sit", Animation.LoopType.HOLD_ON_LAST_FRAME));
+            return PlayState.CONTINUE;
+        } else if (geoAnimatableAnimationState.isMoving()) {
             geoAnimatableAnimationState.getController().setAnimation(RawAnimation.begin().then("animation.crawler.walk", Animation.LoopType.LOOP));
             return PlayState.CONTINUE;
         }
